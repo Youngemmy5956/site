a9b4789af9cbc3eb8e7c775dffe99b96f03d1f0e
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2022-02-15 02:18:33 +0100

    layout

--- resources/views/Auth/register.blade.php
+++ resources/views/Auth/register.blade.php
@@ -5,24 +5,29 @@
 
         <div class="form-group">
             <label for="">Name</label>
-            <input name="name" value="" required class="form-control">
+            <input name="name" value="{{old('name')}}" required class="form-control">
 
         </div>
 
         <div class="form-group">
             <label for="">E-mail</label>
-            <input name="e-mail" value="" required class="form-control">
+            <input name="e-mail" value="{{old('email')}}" required class="form-control">
 
         </div>
 
         <div class="form-group">
             <label for="">Password</label>
-            <input name="password" value="" required class="form-control">
+            <input name="password" required class="form-control">
 
         </div>
 
         <div class="form-group">
             <label for="">Retype Password</label>
-            <input name="pasword_confirmation" value="" required class="form-control">
+            <input name="pasword_confirmation"  required class="form-control">
+
+        </div>
+
+        <button type="submit" class="btn btn-primary btn-block">Register</button>
+
     </form>
 @endsection('content')
